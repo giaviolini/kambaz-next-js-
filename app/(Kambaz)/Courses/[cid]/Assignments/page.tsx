754584a8f76004efe493c16f972c3ae7abd7a1e5
Gia Violini <g1aviolini123@gmail.com>
2025-10-17 18:59:52 -0400

    a3 javaScript

--- app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
@@ -1,54 +1,46 @@
+"use client"
 import { Badge, ListGroup, ListGroupItem } from "react-bootstrap";
 import { FaPlus } from "react-icons/fa6";
 import AssignmentControls from "./AssignmentControls";
 import { BsGripVertical } from "react-icons/bs";
 import LessonControlButtons from "../Modules/LessonControlButtons";
 import { LuNotebookPen } from "react-icons/lu";
+import { useParams } from "next/navigation";
+import assignments from "../../../Database/assignments.json";
 import Link from "next/link";
 
 export default function Assignments() {
+  const { cid } = useParams();
+  const courseAssignments = assignments.filter(a => a.course === cid);
     return (
       <div>
-      <AssignmentControls /><br /><br /><br /><br />
       <ListGroup className="rounded-0" id="wd-modules">
         <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary"> 
-          <BsGripVertical className="me-2 fs-3" /> ASSIGNMENTS 
-          <FaPlus className="float-end"/> <Badge className="float-end" bg="dark">40% of Total</Badge>
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" />
+            <FaPlus className="float-end" />
+            <Badge className="float-end" bg="dark">40% of Total</Badge>
           </div>
           <ListGroup className="wd-lessons rounded-0">
-            <ListGroupItem className="wd-lesson p-3 ps-1">
-            <BsGripVertical className="me-2 fs-3" /> <LuNotebookPen /> 
-            <Link href="/Courses/1234/Assignments/123"
-             className="wd-assignment-link text-decoration-none">
-             A1 - ENV + HTML
-            </Link> <LessonControlButtons /> 
-            <br></br> Multiple Modules | Not available until MAY 6 AT 12:00AM | <br></br>
-            Due May 13 at 11:59pm | 100 points
-            </ListGroupItem>
-            <ListGroupItem className="wd-lesson p-3 ps-1">
-            <BsGripVertical className="me-2 fs-3" /> <LuNotebookPen /> 
-            <Link href="/Courses/1234/Assignments/123"
-             className="wd-assignment-link text-decoration-none">
-             A2 - HTML + CSS
-            </Link> 
-            <LessonControlButtons />
-            <br></br> Multiple Modules | Not available until MAY 14 AT 12:00AM | <br></br>
-              Due May 23 at 11:59pm | 100 points
-            </ListGroupItem>
-            <ListGroupItem className="wd-lesson p-3 ps-1">
-            <BsGripVertical className="me-2 fs-3" /> <LuNotebookPen /> 
-            <Link href="/Courses/1234/Assignments/123"
-             className="wd-assignment-link text-decoration-none">
-             A3 - JavaScript
-            </Link> 
-            <LessonControlButtons />
-            <br></br> Multiple Modules | Not available until MAY 24 AT 12:00AM | <br></br>
-            Due May 30 at 11:59pm | 100 points
-           </ListGroupItem>
+            {courseAssignments.map(assignment => (
+              <ListGroupItem key={assignment._id} className="wd-lesson p-3 ps-1">
+                <BsGripVertical className="me-2 fs-3" /> <LuNotebookPen />
+                <Link 
+                  href={`/Courses/${cid}/Assignments/${assignment._id}`}
+                  className="wd-assignment-link text-decoration-none"
+                >
+                  {assignment.title}
+                </Link>
+                <LessonControlButtons />
+                <br />
+                {/* Customize these date displays if you have date info */}
+                Multiple Modules | Not available until {assignment.startDate} | <br />
+                Due {assignment.endDate}| 100 points
+              </ListGroupItem>
+            ))}
           </ListGroup>
         </ListGroupItem>
-      </ListGroup>    
+      </ListGroup>   
       </div>
   );}
   
--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -1,37 +1,32 @@
+"use client"
+import { useParams } from "next/navigation";
+import * as db from "../../../Database";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
 import ModulesControls from "./ModulesControls";
 import { BsGripVertical } from "react-icons/bs";
 import LessonControlButtons from "./LessonControlButtons";
 
+
 export default function Modules() {
+  const { cid } = useParams();
+  const modules = db.modules;
     return (
       <div>
-      <ModulesControls /><br /><br /><br /><br />
-      <ListGroup className="rounded-0" id="wd-modules">
-        <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary"> 
-          <BsGripVertical className="me-2 fs-3" /> Week 1  <LessonControlButtons /> 
-          </div>
+  <ModulesControls /><br /><br /><br /><br />
+  <ListGroup className="rounded-0" id="wd-modules">
+  <ListGroup id="wd-modules" className="rounded-0">
+    {modules
+      .filter((module: any) => module.course === cid)
+      .map((module: any) => (
+      <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
+        <div className="wd-title p-3 ps-2 bg-secondary">
+          <BsGripVertical className="me-2 fs-3" /> {module.name} <LessonControlButtons />
+        </div>
+        {module.lessons && (
           <ListGroup className="wd-lessons rounded-0">
-            <ListGroupItem className="wd-lesson p-3 ps-1">
-            <BsGripVertical className="me-2 fs-3" /> LEARNING OBJECTIVES <LessonControlButtons /> </ListGroupItem>
-            <ListGroupItem className="wd-lesson p-3 ps-1">
-            <BsGripVertical className="me-2 fs-3" /> Introduction to the course <LessonControlButtons />
-            </ListGroupItem>
-            <ListGroupItem className="wd-lesson p-3 ps-1">
-              Learn what is Web Development </ListGroupItem>
-          </ListGroup>
-        </ListGroupItem>
-        <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary"> Week 2 </div>
-          <ListGroup className="wd-lessons rounded-0">
-            <ListGroupItem className="wd-lesson p-3 ps-1">
-              LESSON 1 </ListGroupItem>
-            <ListGroupItem className="wd-lesson p-3 ps-1">
-              LESSON 2 </ListGroupItem>
-          </ListGroup>
-        </ListGroupItem>
-      </ListGroup>    
-      </div>
-  );}
-  
+            {module.lessons.map((lesson: any) => (
+              <ListGroupItem className="wd-lesson p-3 ps-1">
+                <BsGripVertical className="me-2 fs-3" /> {lesson.name} <LessonControlButtons />
+              </ListGroupItem>))}</ListGroup>)}</ListGroupItem>))} </ListGroup>
+    </ListGroup>
+    </div> );}
