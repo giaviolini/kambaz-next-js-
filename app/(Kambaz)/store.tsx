9e1160b3eec00d3a473aa4881c9e8c1f6f65253f
Gia Violini <g1aviolini123@gmail.com>
2025-10-30 23:57:20 -0400

    assignment 4 - vercel fix part 3

--- app/(Kambaz)/store.tsx
+++ app/(Kambaz)/store.tsx
@@ -7,3 +7,5 @@ const store = configureStore({
  reducer: { coursesReducer,  modulesReducer, accountReducer, assignmentsReducer, },
 });
 export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
