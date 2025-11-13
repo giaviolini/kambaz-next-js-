2ce69191b57df26732187aefb6f4650bfc337116
Gia Violini <g1aviolini123@gmail.com>
2025-11-13 14:17:35 -0500

    assignment 5

---
+++ kambaz-node-server-app-F25/Kambaz/Courses/routes.js
@@ -0,0 +1,41 @@
+import * as dao from "./dao.js";
+import * as modulesDao from "../Modules/dao.js";
+export default function CourseRoutes(app) {
+  const findAllCourses = (req, res) => {
+    const courses = dao.findAllCourses();
+    res.send(courses);
+  }
+
+  const deleteCourse = (req, res) => {
+    const { courseId } = req.params;
+    const status = dao.deleteCourse(courseId);
+    res.send(status);
+  }
+
+  const updateCourse = (req, res) => {
+    const { courseId } = req.params;
+    const courseUpdates = req.body;
+    const status = dao.updateCourse(courseId, courseUpdates);
+    res.send(status);
+  }
+  const findModulesForCourse = (req, res) => {
+    const { courseId } = req.params;
+    const modules = modulesDao.findModulesForCourse(courseId);
+    res.json(modules);
+  }
+  const createModuleForCourse = (req, res) => {
+    const { courseId } = req.params;
+    const module = {
+      ...req.body,
+      course: courseId,
+    };
+    const newModule = modulesDao.createModule(module);
+    res.send(newModule);
+  }
+
+  app.delete("/api/courses/:courseId", deleteCourse);
+  app.get("/api/courses", findAllCourses);
+  app.put("/api/courses/:courseId", updateCourse);
+  app.get("/api/courses/:courseId/modules", findModulesForCourse);
+  app.post("/api/courses/:courseId/modules", createModuleForCourse);
+}
---
+++ kambaz-node-server-app-F25/index.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import Hello from "./Hello.js"
+import Lab5 from "./Lab5/index.js";
+import cors from "cors";
+import UserRoutes from "./Kambaz/Users/routes.js";
+import CourseRoutes from "./Kambaz/Courses/routes.js";
+import session from "express-session";
+import ModuleRoutes from "./Kambaz/Modules/routes.js";
+import EnrollmentRoutes from "./Kambaz/Enrollments/routes.js"
+import "dotenv/config";
+const app = express();
+UserRoutes(app);
+app.use(cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",})); 
+const sessionOptions = {
+        secret: process.env.SESSION_SECRET || "kambaz",
+        resave: false,
+        saveUninitialized: false,
+      };
+      if (process.env.SERVER_ENV !== "development") {
+        sessionOptions.proxy = true;
+        sessionOptions.cookie = {
+          sameSite: "none",
+          secure: true,
+          domain: process.env.SERVER_URL,
+        };
+      }
+app.use(session(sessionOptions));     
+app.use(express.json());
+UserRoutes(app);
+CourseRoutes(app);
+EnrollmentRoutes(app);
+ModuleRoutes(app);
+Hello(app)
+Lab5(app);
+app.listen(process.env.PORT || 4000)
