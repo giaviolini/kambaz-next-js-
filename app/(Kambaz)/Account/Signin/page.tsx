421777c1e3276d6907d2ebf718a6cd9f832942d9
Gia Violini <g1aviolini123@gmail.com>
2025-12-08 17:18:30 -0500

    server connectivity

--- app/(Kambaz)/Account/Signin/page.tsx
+++ app/(Kambaz)/Account/Signin/page.tsx
@@ -1,34 +1,52 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import Link from "next/link";
-import { redirect } from "next/dist/client/components/navigation";
+import { useRouter } from "next/navigation";
 import { setCurrentUser } from "../reducer";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
-import * as db from "../../Database";
-import { FormControl, Button } from "react-bootstrap";
 import * as client from "../client";
+import { FormControl, Button } from "react-bootstrap";
 
 export default function Signin() {
-  const [credentials, setCredentials] = useState<any>({});
- const dispatch = useDispatch();
- const signin = async () => {
-   const user = await client.signin(credentials);
-   if (!user) return;
-   dispatch(setCurrentUser(user));
-   redirect("/Dashboard");
- };
+  const [credentials, setCredentials] = useState<any>({ username: "", password: "" });
+  const dispatch = useDispatch();
+  const router = useRouter();
 
- return (
-   <div id="wd-signin-screen">
-     <h1>Sign in</h1>
-     <FormControl defaultValue={credentials.username}
-             onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
-             placeholder="username" id="wd-username" className="mb-2"/>
-     <FormControl defaultValue={credentials.password}
-             onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
-             placeholder="password" type="password" id="wd-password" className="mb-2"/>
-     <Button onClick={signin} id="wd-signin-btn" className="btn btn-primary w-100 mb-2"> Sign in </Button>
-     <Link href="Signup" id="wd-signup-link"> Sign up </Link>
-   </div>
-);}
+  const signin = async () => {
+    try {
+      const user = await client.signin(credentials);
+      if (!user) return;
+      dispatch(setCurrentUser(user));
+      router.push("/Dashboard");
+    } catch (error) {
+      console.error("Signin error:", error);
+      alert("Sign in failed. Please check your credentials.");
+    }
+  };
+
+  return (
+    <div id="wd-signin-screen">
+      <h1>Sign in</h1>
+      <FormControl 
+        value={credentials.username}  // Changed from defaultValue to value
+        onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
+        placeholder="username" 
+        id="wd-username" 
+        className="mb-2"
+      />
+      <FormControl 
+        value={credentials.password}  // Changed from defaultValue to value
+        onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+        placeholder="password" 
+        type="password" 
+        id="wd-password" 
+        className="mb-2"
+      />
+      <Button onClick={signin} id="wd-signin-btn" className="btn btn-primary w-100 mb-2">
+        Sign in
+      </Button>
+      <Link href="/Signup" id="wd-signup-link">Sign up</Link>
+    </div>
+  );
+}
