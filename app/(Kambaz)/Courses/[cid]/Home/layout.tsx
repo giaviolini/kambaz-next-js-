d7cee875295b806324e81c497e70cc623c04b84b
Gia Violini <g1aviolini123@gmail.com>
2025-12-11 22:22:03 -0500

    modules layout

---
+++ app/(Kambaz)/Courses/[cid]/Home/layout.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+export default function CourseLayout({ children }: { children: ReactNode }) {
+  return <>{children}</>; // or wrap in a div/layout as needed
+}
