39c04174f9c85ba3e1fe01105c700da32f50b313
Gia Violini <g1aviolini123@gmail.com>
2025-12-11 22:27:01 -0500

    courses layout

--- app/(Kambaz)/Courses/layout.tsx
+++ app/(Kambaz)/Courses/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
 
 export default function CourseLayout({ children }: { children: ReactNode }) {
-  return <>{children}</>; // or wrap in a div/layout as needed
+  return <>{children}</>;
 }
