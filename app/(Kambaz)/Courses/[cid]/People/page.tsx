e03f169638300ddf96a4627587b5c487eb123f56
Gia Violini <g1aviolini123@gmail.com>
2025-12-12 13:01:27 -0500

    people table

--- app/(Kambaz)/Courses/[cid]/People/page.tsx
+++ app/(Kambaz)/Courses/[cid]/People/page.tsx
@@ -1,32 +1,24 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client"
-import { Table } from "react-bootstrap";
-import { FaUserCircle } from "react-icons/fa";
-import * as accountClient from "../../../Account/client";
-import PeopleDetails from "./Details";
 import { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
+import PeopleTable from "./PeopleTable";
+import axios from "axios";
 
-// You need a client function to get enrolled users - add this import
-// import * as enrollmentsClient from "./client"; 
+const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 
-const ENROLLMENTS_API = `${process.env.NEXT_PUBLIC_HTTP_SERVER}/api/courses`;
-
-export default function PeopleTable() {
+export default function PeoplePage() {
   const { cid } = useParams();
   const [users, setUsers] = useState<any[]>([]);
-  const [showDetails, setShowDetails] = useState(false);
-  const [showUserId, setShowUserId] = useState<string | null>(null);
 
   const fetchUsers = async () => {
     try {
-      const response = await fetch(`${ENROLLMENTS_API}/${cid}/users`, {
-        credentials: 'include'
+      const response = await axios.get(`${HTTP_SERVER}/api/courses/${cid}/users`, {
+        withCredentials: true
       });
-      const data = await response.json();
-      setUsers(data);
+      setUsers(response.data);
     } catch (error) {
-      console.error("Error fetching users:", error);
+      console.error("Error fetching enrolled users:", error);
     }
   };
 
@@ -37,52 +29,9 @@ export default function PeopleTable() {
   }, [cid]);
 
   return (
-    <div id="wd-people-table">
-      {showDetails && (
-        <PeopleDetails
-          uid={showUserId}
-          onClose={() => {
-            setShowDetails(false);
-            fetchUsers();
-          }}
-        />
-      )}
-      <Table striped>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Login ID</th>
-            <th>Section</th>
-            <th>Role</th>
-            <th>Last Activity</th>
-            <th>Total Activity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user: any) => (
-            <tr key={user._id}>
-              <td className="wd-full-name text-nowrap">
-                <span
-                  className="text-decoration-none"
-                  onClick={() => {
-                    setShowDetails(true);
-                    setShowUserId(user._id);
-                  }}
-                >
-                  <FaUserCircle className="me-2 fs-1 text-secondary" />
-                  <span className="wd-first-name">{user.firstName}</span>{" "}
-                  <span className="wd-last-name">{user.lastName}</span>
-                </span>
-              </td>
-              <td className="wd-login-id">{user.loginId}</td>
-              <td className="wd-section">{user.section}</td>
-              <td className="wd-role">{user.role}</td>
-              <td className="wd-last-activity">{user.lastActivity}</td>
-              <td className="wd-total-activity">{user.totalActivity}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+    <div>
+      <h3>People</h3>
+      <PeopleTable users={users} fetchUsers={fetchUsers} />
     </div>
   );
 }
